c2de61b31d1edd3e0393e492cca09ece6ad90660
海津宏基 <kaidzuhiroki@kaidzuhirokinoMacBook-Air.local>
2022-03-27 22:25:36 +0900

    [add]GUIの配置

--- omikuji.py
+++ omikuji.py
@@ -12,7 +12,15 @@ canvas = tkinter.Canvas(root,width=800,height=600)
 canvas.pack()
 #画像の読み込み
 gazou = tkinter.PhotoImage(file="miko.png")
-#キャンバスに画像を描写
+#キャンバスに画像を描画
 canvas.create_image(400,300,image=gazou)
+#ラベルの部品を作る
+label = tkinter.Label(root,text="??",font=("Times New Roman",120),bg="white")
+#ラベルを配置
+label.place(x=380,y=60)
+#ボタンの部品を作る
+button = tkinter.Button(root,text="おみくじを引く",font=("Times New Roman",36),fg="skyblue")
+#ボタンを配置
+button.place(x=360,y=400)
 #ウィンドウを表示
 root.mainloop()
