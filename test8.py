64f3ed9df287bcf75169c3c526e9fc16833962b9
海津宏基 <kaidzuhiroki@kaidzuhirokinoMacBook-Air.local>
2022-03-27 23:25:41 +0900

    [add]チェックの有無を知る

--- test8.py
+++ test8.py
@@ -3,11 +3,15 @@ import tkinter
 #ウィンドウのオブジェクトを作る
 root = tkinter.Tk()
 #タイトルを指定
-root.title("チェックボタンを扱う")
+root.title("最初からチェックされた状態にする")
 #サイズを指定
 root.geometry("400x200")
+#BoolleanVar()のオブジェくトを用意
+cval = tkinter.BooleanVar()
+#それをTrueセット
+cval.set(True)
 #チェックボタンの部品を作成
-cbtn = tkinter.Checkbutton(text="チェックボタン")
+cbtn = tkinter.Checkbutton(text="チェックボタン",variable=cval)
 #チェックボタンの部品を配置
 cbtn.pack()
 #ウィンドウを表示
