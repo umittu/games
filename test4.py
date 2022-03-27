c7c2b81d3bb45c775ad8ff389e297504f527a23c
海津宏基 <kaidzuhiroki@kaidzuhirokinoMacBook-Air.local>
2022-03-27 16:10:11 +0900

    [add]ラベルの配置

--- test4.py
+++ test4.py
@@ -7,5 +7,10 @@ root = tkinter.Tk()
 root.title("初めてのウィンドウ")
 #ウィンドウサイズを指定
 root.geometry("800x600")
+#ラベル部品を作る
+label = tkinter.Label(root,text="ラベルの文字列",
+font=("System",24))
+#ウィンドウにラベルを配置
+label.place(x=200,y=100)
 #ウィンドウを表示
 root.mainloop()
