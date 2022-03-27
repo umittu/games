723f0ec78fc6d46b49720826344b2353383a21f2
海津宏基 <kaidzuhiroki@kaidzuhirokinoMacBook-Air.local>
2022-03-27 16:39:00 +0900

    [add]キャンバスに画像を表示する

--- test6.py
+++ test6.py
@@ -3,11 +3,15 @@ import tkinter
 #ウィンドウオブジェクトを作る
 root = tkinter.Tk()
 #ウィンドウのタイトルを指定
-root.title("初めてのキャンバス")
+root.title("初めての画像表示")
 #キャンバス部品を作る
-canvas = tkinter.Canvas(root,width=400,height=600,
+canvas = tkinter.Canvas(root,width=800,height=800,
 bg="skyblue")
 #ウィンドウにキャンバスを配置
 canvas.pack()
+#gazouに画像ファイルを読み込む
+gazou = tkinter.PhotoImage(file="otter.png")
+#キャンバスに画像を描画
+canvas.create_image(400,400,image=gazou)
 #ウィンドウを表示
 root.mainloop()
