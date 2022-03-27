70f970fa6237610475db0b08cb6ca95acb7604d7
海津宏基 <kaidzuhiroki@kaidzuhirokinoMacBook-Air.local>
2022-03-27 16:22:33 +0900

    [add]ボタンをクリックしたときの反応

--- test4.py
+++ test4.py
@@ -1,15 +1,20 @@
 #tkinterモジュールをインポート
 import tkinter
 
+#関数の宣言
+def click_btn():
+    #ボタンの文字列変更
+    button["text"] = "クリックしました"
+
 #ウィンドウの部品（オブジェクト）を作る
 root = tkinter.Tk()
 #ウィンドウのタイトルを指定
 root.title("初めてのウィンドウ")
 #ウィンドウサイズを指定
 root.geometry("800x600")
-#ボタンの部品を作る
+#ボタンの部品を作る際、command=でクリック時に動く関数を指定
 button = tkinter.Button(root,text="ボタンの文字列",
-font=("Times New Roman",24))
+font=("Times New Roman",24),command=click_btn)
 #ウィンドウにボタンを配置
 button.place(x=400,y=200)
 #ラベル部品を作る
