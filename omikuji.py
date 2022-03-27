7719d67de7ab818b03cf9dd7c4c4567ea88e6e34
海津宏基 <kaidzuhiroki@kaidzuhirokinoMacBook-Air.local>
2022-03-27 22:35:15 +0900

    [add]ボタンを反応させる

--- omikuji.py
+++ omikuji.py
@@ -1,5 +1,13 @@
-#tkinterモジュールをインポート
+#tkinter,randomモジュールをインポート
 import tkinter
+import random
+
+#ボタンをクリックしたときの関数を定義
+#ラベルの文字をランダムに変更
+def click_btn():
+    label["text"]=random.choice(["大吉","中吉","小吉","凶"])
+    label.iupdate()
+
 #ウィンドウのオブジェクトを作る
 root = tkinter.Tk()
 #タイトルを指定
@@ -18,8 +26,8 @@ canvas.create_image(400,300,image=gazou)
 label = tkinter.Label(root,text="??",font=("Times New Roman",120),bg="white")
 #ラベルを配置
 label.place(x=380,y=60)
-#ボタンの部品を作る
-button = tkinter.Button(root,text="おみくじを引く",font=("Times New Roman",36),fg="skyblue")
+#ボタンの部品を作る,commandでクリック時に動く関数を設定
+button = tkinter.Button(root,text="おみくじを引く",font=("Times New Roman",36),command=click_btn,fg="skyblue")
 #ボタンを配置
 button.place(x=360,y=400)
 #ウィンドウを表示
