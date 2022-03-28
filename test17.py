d9f3feb26058d499310753dab363fe550ae93ba8
海津宏基 <kaidzuhiroki@kaidzuhirokinoMacBook-Air.local>
2022-03-28 21:43:46 +0900

    [add]判定用のリストを使う

--- test17.py
+++ test17.py
@@ -17,8 +17,11 @@ def mouse_press(e):
     mouse_c=1
 
 neko=[]
+check=[]
+
 for i in range(10):
     neko.append([0,0,0,0,0,0,0,0])
+    check.append([0,0,0,0,0,0,0,0])
 
 def draw_neko():
     for y in range(10):
@@ -26,20 +29,40 @@ def draw_neko():
             if neko[y][x]>0:
                 cvs.create_image(x*72+60,y*72+60,image=img_neko[neko[y][x]],tag="NEKO")
 
-def yoko_neko():
+def check_neko():
+    for y in range(10):
+        for x in range(8):
+            check[y][x]=neko[y][x]
+    for y in range(1,9):
+        for x in range(8):
+            if check[y][x]>0:
+                if check[y-1][x]==check[y][x] and check[y+1][x]==check[y][x]:
+                    neko[y-1][x]=7
+                    neko[y][x]=7
+                    neko[y+1][x]=7
     for y in range(10):
         for x in range(1,7):
-            if neko[y][x]>0:
-                if neko[y][x-1]==neko[y][x] and neko[y][x+1]==neko[y][x]:
+            if check[y][x]>0:
+                if check[y][x-1]==check[y][x] and check[y][x+1]==check[y][x]:
                     neko[y][x-1]=7
                     neko[y][x]=7
                     neko[y][x+1]=7
-
+    for y in range(1,9):
+        for x in range(1,7):
+            if check[y][x]>0:
+                if check[y-1][x-1]==check[y][x] and check[y+1][x+1]==check[y][x]:
+                    neko[y-1][x-1]=7
+                    neko[y][x]=7
+                    neko[y+1][x+1]=7 
+                if check[y+1][x-1]==check[y][x] and check[y-1][x+1]==check[y][x]:
+                    neko[y+1][x-1]=7
+                    neko[y][x]=7
+                    neko[y-1][x+1]=7 
 def game_main():
     global cursor_x,cursor_y,mouse_c
     if 660<=mouse_x and mouse_x<840 and 100<=mouse_y and mouse_y<160 and mouse_c==1:
         mouse_c=0
-        yoko_neko()
+        check_neko()
     if 24<=mouse_x and mouse_x<24+72*8 and 24<=mouse_y and mouse_y<24+72*10:
         cursor_x=int((mouse_x-24)/72)
         cursor_y=int((mouse_y-24)/72)
@@ -53,7 +76,7 @@ def game_main():
     root.after(100,game_main)
 
 root=tkinter.Tk()
-root.title("横に3つ並んだか")
+root.title("縦横斜めに3つ並んだか")
 root.resizable(False,False)
 root.bind("<Motion>",mouse_move)
 root.bind("<ButtonPress>",mouse_press)
